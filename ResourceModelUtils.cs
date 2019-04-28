6547ef9192e3f304c219df78d593a0f39ff67d40
Askild Olsen <askild@xangeli.com>
2019-04-28 18:45:29 +0200

    Utility for Geohash av WKT

--- ResourceModelUtils.cs
+++ ResourceModelUtils.cs
@@ -14,5 +14,33 @@ namespace resource_etl
         {
             return Sparrow.Hashing.XXHash64.Calculate(key, System.Text.Encoding.UTF8).ToString();
         }
+
+        public static IEnumerable<string> WKTEncodeGeohash(string wkt, int precision)
+        {
+            var geometry = new NetTopologySuite.IO.WKTReader().Read(wkt);
+
+            var geohashsize = Spatial4n.Core.Util.GeohashUtils.DecodeBoundary(Spatial4n.Core.Util.GeohashUtils.EncodeLatLon(geometry.InteriorPoint.Y, geometry.InteriorPoint.X, precision), Spatial4n.Core.Context.SpatialContext.GEO);
+
+            var shapeFactory = new NetTopologySuite.Utilities.GeometricShapeFactory();
+            shapeFactory.Width = geohashsize.GetWidth();
+            shapeFactory.Height = geohashsize.GetHeight();
+            shapeFactory.NumPoints = 4;
+
+            for (double y = geometry.EnvelopeInternal.MinY; y <= geometry.EnvelopeInternal.MaxY; y += geohashsize.GetHeight())
+            {
+                for (double x = geometry.EnvelopeInternal.MinX; x <= geometry.EnvelopeInternal.MaxX; x += geohashsize.GetWidth())
+                {
+                    var geohash = Spatial4n.Core.Util.GeohashUtils.EncodeLatLon(y, x, precision);
+                    var geohashdecoded = Spatial4n.Core.Util.GeohashUtils.Decode(geohash, Spatial4n.Core.Context.SpatialContext.GEO);
+
+                    shapeFactory.Centre = new GeoAPI.Geometries.Coordinate(geohashdecoded.GetX(), geohashdecoded.GetY());
+
+                    if (shapeFactory.CreateRectangle().Intersects(geometry))
+                    {
+                        yield return geohash;
+                    }
+                }
+            }
+        }
     }
 }
