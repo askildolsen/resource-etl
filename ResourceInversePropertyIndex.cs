9c5fc001ce63944f620ded2cbd6e314e7bf856ea
Askild Olsen <askild@xangeli.com>
2019-07-01 19:41:45 +0200

    Fjern overflødige using

--- ResourceInversePropertyIndex.cs
+++ ResourceInversePropertyIndex.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Raven.Client.Documents.Indexes;
-using Raven.Client.Documents.Linq.Indexing;
 using static resource_etl.ResourceModel;
 using static resource_etl.ResourceModelUtils;
 
