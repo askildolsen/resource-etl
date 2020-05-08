154e388da1ae185718cdb881cabdf7137429988c
Askild Olsen <askild@xangeli.com>
2020-05-08 20:53:59 +0200

    Ta med Title, SubTitle og Code i Resource

--- ResourcePropertyIndex.cs
+++ ResourcePropertyIndex.cs
@@ -14,12 +14,14 @@ namespace resource_etl
             AddMap<ResourceOntology>(ontologies =>
                 from ontology in ontologies
                 from resource in LoadDocument<ResourceMapped>(ontology.Source).Where(r => r != null)
-                select new Resource
-                {
+                select new Resource {
                     Context = ontology.Context,
                     ResourceId = resource.ResourceId,
                     Type = resource.Type,
                     SubType = resource.SubType,
+                    Title = resource.Title,
+                    SubTitle = resource.SubTitle,
+                    Code = resource.Code,
                     Status = resource.Status,
                     Tags = resource.Tags,
                     Properties = (IEnumerable<Property>)Properties(
@@ -40,12 +42,14 @@ namespace resource_etl
             Reduce = results =>
                 from result in results
                 group result by new { result.Context, result.ResourceId } into g
-                select new Resource
-                {
+                select new Resource {
                     Context = g.Key.Context,
                     ResourceId = g.Key.ResourceId,
                     Type = g.SelectMany(r => r.Type).Distinct(),
                     SubType = g.SelectMany(r => r.SubType).Distinct(),
+                    Title = g.SelectMany(r => r.Title).Distinct(),
+                    SubTitle = g.SelectMany(r => r.SubTitle).Distinct(),
+                    Code = g.SelectMany(r => r.Code).Distinct(),
                     Status = g.SelectMany(r => r.Status).Distinct(),
                     Tags = g.SelectMany(r => r.Tags).Distinct(),
                     Properties = g.SelectMany(r => r.Properties),
